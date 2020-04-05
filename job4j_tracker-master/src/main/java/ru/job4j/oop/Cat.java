ea834c47490af1dc20d4c0a1b5f14d6078fce5f0
Max <meonoeek@gmail.com>
2020-04-05 18:06:22 +0300

    1.5. Поля объекта[#264233]

--- job4j_tracker-master/src/main/java/ru/job4j/oop/Cat.java
+++ job4j_tracker-master/src/main/java/ru/job4j/oop/Cat.java
@@ -2,14 +2,27 @@ package ru.job4j.oop;
 
 public class Cat {
 
-    public String sound() {
-        String voice = "may-may";
-        return voice;
-    }
+    private String name;
+    private String food;
 
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        String say = peppy.sound();
-        System.out.println("Peppy says " + say);
+        Cat gav = new Cat();
+        gav.giveNick("gav");
+        gav.eat("kotleta");
+        gav.show();
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void show() {
+        System.out.println("Name cat: " + this.name);
+        System.out.println("Food cat: " + this.food);
+
     }
 }
