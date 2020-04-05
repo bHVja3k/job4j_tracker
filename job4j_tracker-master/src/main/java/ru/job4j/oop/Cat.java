ae6db876834a047ff4a7eea237f75f4d9b24ef32
Max <meonoeek@gmail.com>
2020-04-05 16:25:43 +0300

    1.1. Создание объекта[#264232]

---
+++ job4j_tracker-master/src/main/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,10 @@
+package ru.job4j.oop;
+
+public class Cat {
+    public static void main(String[] args) {
+        // Создаем обьект класса Cat
+        // Здесь мы создаем переменную типа Cat с именем peppy
+        Cat peppy = new Cat();
+        Cat sparky = new Cat();
+    }
+}
