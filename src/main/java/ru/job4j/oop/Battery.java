aba6f603e7f9a26bf058e03d29953ba769815356
Max <meonoeek@gmail.com>
2020-04-08 13:23:06 +0300

    1.7. Состояние объекта.[#264238]

--- src/main/java/ru/job4j/oop/Battery.java
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -8,13 +8,13 @@ public class Battery {
     }
 
     public void exchange(Battery another) {
-        this.value = this.value - this.value + another.value;
+        this.value = this.value + another.value;
         another.value = 0;
     }
 
     public static void main(String[] args) {
-        Battery first = new Battery(10);
-        Battery second = new Battery(5);
+        Battery first = new Battery(50);
+        Battery second = new Battery(50);
         System.out.println("first : " + first.value + ". second : " + second.value);
         first.exchange(second);
         System.out.println("first : " + first.value + ". second : " + second.value);
