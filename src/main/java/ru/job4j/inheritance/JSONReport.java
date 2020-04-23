caaec19e3c8e289f3e1ef6862741a084c18b402c
Max <meonoeek@gmail.com>
2020-04-23 15:52:33 +0300

    5. Аннотация @Override[#264246]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -1,6 +1,7 @@
 package ru.job4j.inheritance;
 
 public class JSONReport extends TextReport {
+    @Override
         public static void main(String[] args) {
             JSONReport report = new JSONReport();
             String text = report.generate("name", "body");
