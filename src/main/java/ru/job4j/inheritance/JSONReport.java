d059993c33a41eaca836670a244ad2ad1cc43b52
Max <meonoeek@gmail.com>
2020-04-23 16:51:13 +0300

    2. Модель данных.[#264250]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -1,10 +1,10 @@
 package ru.job4j.inheritance;
 
 public class JSONReport extends TextReport {
-        public static void main(String[] args) {
-            JSONReport report = new JSONReport();
-            String text = report.generate("name", "body");
-            System.out.println(text);
-        }
-
+    @Override
+    public String generate(String name, String body) {
+        return "<h1>" + name + "</h1>" +
+                "<br/>" +
+                "<span>" + body + "</span>";
+    }
 }
