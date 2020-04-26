bb3620dd305a298c64322c690523f113798da6bc
Max <meonoeek@gmail.com>
2020-04-26 18:57:55 +0300

    6. Метод замены заявки. Tracker.replace[#264254]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -23,15 +23,8 @@ public class Tracker {
     }
 
     public Item findById(String id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId().equals(id)) {
-                rsl = item;
-                break;
-            }
-        }
-        return rsl;
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
     }
 
     public Item findByName(String name) {
@@ -52,5 +45,28 @@ public class Tracker {
             rsl = items[index];
         }
         return rsl;
+    }
+
+    private int indexOf(String id) {
+        int rsl = -1;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getId().equals(id)) {
+                rsl = index;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    /**
+     * Метод замены заявки в хранилище (id заявки сохраняется)
+     * @param id заявки, которую нужно заменить
+     * @param item новая заявка
+     */
+    public boolean replace(String id, Item item) {
+        int index = indexOf(id);
+        item.setId(id);
+        this.items[index] = item;
+        return true;
     }
 }
