99ee0786f410f3ade62f06191b96243c5e99ba8c
Max <meonoeek@gmail.com>
2020-05-28 18:24:06 +0300

    4. Переопределение[174890#264245]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -2,8 +2,6 @@ package ru.job4j.inheritance;
 
 public class JSONReport extends TextReport {
     public String generate(String name, String body) {
-        return "<h1>" + name + "</h1>" +
-                "<br/>" +
-                "<span>" + body + "</span>";
+        return "name: " + name + System.lineSeparator() + "body: " + body;
     }
 }
