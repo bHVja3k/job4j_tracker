c133721b23fbf2fd96dd872bee240740123d52f9
Max <meonoeek@gmail.com>
2020-06-03 01:26:55 +0300

    3. Интерфейс Input[181092#264200]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,19 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+
+    @Override
+    public String go() {
+        return null;
+    }
+
+    @Override
+    public String num(String passengers) {
+        return null;
+    }
+
+    @Override
+    public int fill(int fuel) {
+        return 0;
+    }
+}
---
+++ src/main/java/ru/job4j/poly/Transport.java
@@ -0,0 +1,7 @@
+package ru.job4j.poly;
+
+public interface Transport {
+    String go();
+    String num (String passengers);
+    int fill(int fuel);
+}
