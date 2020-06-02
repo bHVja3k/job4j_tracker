3b8486a4ad0b2a5a9f0c8caf93702082027517da
Max <meonoeek@gmail.com>
2020-06-02 23:56:12 +0300

    3. Удаление моделей из массива.[176470#264253]

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -4,44 +4,38 @@ import java.util.Scanner;
 
 public class Matches {
     public static void main(String[] args) {
-        Matches matches = new Matches();
-        int match = 11;
-        System.out.println("Игра 11");
         Scanner input = new Scanner(System.in);
-        boolean run = true;
-        while (run) {
-            System.out.print("Первый игрок берет от 1 до 3 спичек: ");
-            int one = Integer.valueOf(input.nextLine());
-            if(matches.range(one)){
-                System.out.println(one + " это не чсило от 1 до 3!\nЕще раз:");
-                one = Math.abs(input.nextInt());
+        int matchesTotal = 11;
+        int answerCount = 0;
+        int matchesPlayerOne = 0;
+        int matchesPlayerTWo = 0;
+
+        while (true) {
+            if (matchesTotal <= 0) {
+                if (matchesPlayerOne > matchesPlayerTWo) {
+                    System.out.println("Победил игрок 1");
+                } else {
+                    System.out.println("Победил игрок 2");
+                }
+                break;
             }
-
-
-
-            System.out.print("Второй игрок берет от 1 до 3 спичек: ");
-            int two = Integer.valueOf(input.nextLine());
-            while (two < 1 || two > 3) {
-                System.out.println(two + " это не чсило от 1 до 3!\nЕще раз:");
-                two = Math.abs(input.nextInt());
+            System.out.println("Введите число от 1 до 3: ");
+            int answer = Integer.valueOf(input.nextLine());
+            if (answer < 0 || answer > 3) {
+                System.out.println("Вы ввели неверное число, придется начать сначала :(");
+                break;
             }
-            int balance = match - (one + two);
-            match = balance;
-            System.out.println("Осталось спичек: " + balance);
-            System.out.println();
-            if (match <= 0) {
-                run = false;
+            if (answerCount % 2 == 0) {
+                matchesPlayerOne++;
+                answerCount++;
+                matchesTotal -= answer;
+                System.out.println("После ответа первого игрока на столе осталось " + matchesTotal + " спичек");
+            } else {
+                matchesPlayerTWo++;
+                answerCount++;
+                matchesTotal -= answer;
+                System.out.println("После ответа второго игрока на столе осталось " + matchesTotal + " спичек");
             }
         }
-    }
-
-    public boolean range(int num) {
-        boolean range = false;
-        if (num < 1 || num > 3) {
-            range = true;
-        } else {
-            System.out.println(num + " это не число от 1 до 3!\nЕще раз:");
-        }
-        return range;
     }
 }
--- src/main/java/ru/job4j/pojo/Shop.java
+++ src/main/java/ru/job4j/pojo/Shop.java
@@ -29,6 +29,7 @@ public class Shop {
     public Product[] delete(Product[] products, int index) {
         for (int i = index; i < products.length - 1; i++) {
             products[i] = products[i + 1];
+            products[products.length - 1] = null;
         }
         return products;
     }
