0d50d45c34b7b54165ffa40a86f46386450b87af
meonoeek@gmail.com <meonoeek@gmail.com>
2020-06-30 14:29:03 +0300

    1. Организовать сортировку User[10034#264173]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,6 +1,8 @@
 package ru.job4j.tracker;
 
-public class Item {
+import java.util.Objects;
+
+public class Item{
     private String id;
     private String name;
 
@@ -27,5 +29,20 @@ public class Item {
     @Override
     public String toString() {
         return this.name;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Item)) return false;
+        Item item = (Item) o;
+        return Objects.equals(getId(), item.getId()) &&
+                Objects.equals(getName(), item.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName());
     }
 }
