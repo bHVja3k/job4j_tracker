d4bcdc5edac605ba832e75535ea390cec4d15fd2
meonoeek@gmail.com <meonoeek@gmail.com>
2020-07-04 23:18:53 +0300

    Отсортировать департаменты[34352#264168]

---
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -0,0 +1,14 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class DepDescComp implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+        String[] arResultO1 = o1.split("/");
+        String[] arResultO2 = o2.split("/");
+
+        int rst = arResultO2[0].compareTo(arResultO1[0]);
+        return rst == 0 ? o1.compareTo(o2) : rst;
+    }
+}
