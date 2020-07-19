192354f70defa83def21ae97668e3574822f31c0
meonoeek@gmail.com <meonoeek@gmail.com>
2020-07-19 21:04:04 +0300

    0. Stream API[58166#264044]

---
+++ src/main/java/ru/job4j/stream/StreamUsage.java
@@ -0,0 +1,21 @@
+package ru.job4j.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamUsage {
+    public static void main(String[] args) {
+        ArrayList<Integer> tasks = new ArrayList<>();
+        tasks.add(1);
+        tasks.add(-1);
+        tasks.add(2);
+        tasks.add(-2);
+        tasks.add(3);
+        tasks.add(-3);
+        List<Integer> bugs = tasks.stream().filter(
+                task -> task >= 0
+        ).collect(Collectors.toList());
+        bugs.forEach(System.out::println);
+    }
+}
