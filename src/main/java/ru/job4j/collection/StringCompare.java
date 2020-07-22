9978af57923b68aac854567e2e59e2aa0f84af15
meonoeek@gmail.com <meonoeek@gmail.com>
2020-07-22 22:42:11 +0300

    3. Компаратор для строк[35008#264171]

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -10,10 +10,12 @@ public class StringCompare implements Comparator<String> {
             rsl = Character.compare(left.charAt(i), right.charAt(i));
             if (rsl != 0) {
                 break;
-            } else {
-                rsl = Integer.compare(left.length(), right.length());
             }
         }
+        if (rsl == 0) {
+            rsl = Integer.compare(left.length(), right.length());
+        }
         return rsl;
+
     }
 }
