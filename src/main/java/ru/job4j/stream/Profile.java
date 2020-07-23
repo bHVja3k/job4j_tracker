3a735ee5483ac53c3f33824929878a7f8b68edd0
meonoeek@gmail.com <meonoeek@gmail.com>
2020-07-23 22:42:53 +0300

    2. Список адресов[110062#264046]

--- src/main/java/ru/job4j/stream/Profile.java
+++ src/main/java/ru/job4j/stream/Profile.java
@@ -1,23 +1,18 @@
 package ru.job4j.stream;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Profile {
-    private static Address address;
+    private Address address;
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
 
     public Address getAddress() {
-        return address;
+        return this.address;
     }
 
-    List<Address> collect(List<Profile> profiles) {
-        return profiles.stream()
-                .map(Profile::getAddress)
-                .sorted(Comparator.comparing(Address::getCity))
-                .distinct()
-                .collect(Collectors.toList());
-    }
+
 }
-
-
