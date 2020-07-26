faa5243e7042c178453d301bdd37132b01740bcb
meonoeek@gmail.com <meonoeek@gmail.com>
2020-07-26 22:08:40 +0300

    0. Stream API улучшения[58167#264053]

--- src/main/java/ru/job4j/stream/StudentLevel.java
+++ src/main/java/ru/job4j/stream/StudentLevel.java
@@ -10,7 +10,6 @@ public class StudentLevel {
     public static List<Student> levelOf(List<Student> students, int bound) {
         return students.stream()
                 .filter(Objects::nonNull)
-                .filter(st -> st.getScore() > bound)
                 .sorted(Comparator.comparingInt(Student::getScore))
                 .takeWhile(st -> st.getScore() > bound)
                 .collect(Collectors.toList());
